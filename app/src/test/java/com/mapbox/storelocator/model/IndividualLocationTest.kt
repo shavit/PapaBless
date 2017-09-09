babbcaa42edf347e6fbf487df4b2a05d69345d55
shavit <shavit@users.noreply.github.com>
2017-09-09 11:50:57 -0400

    🍕

--- app/src/test/java/com/mapbox/storelocator/model/IndividualLocationTest.kt
+++ app/src/test/java/com/mapbox/storelocator/model/IndividualLocationTest.kt
@@ -1,5 +1,6 @@
 package com.mapbox.storelocator.model
 
+import com.mapbox.mapboxsdk.geometry.LatLng
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -19,7 +20,20 @@ class IndividualLocationTest {
     }
 
     @Test
-    fun getName() {
+    fun createIndividualLocation() {
+        var name: String? = "Location 1"
+        val address = ""
+        val hours = "9am - 4pm"
+        val phoneNum = "(555) 555-9430"
+        val location = LatLng(-73.970048, 40.789752)
+        val mIndividualLocation = IndividualLocation(name, address, hours, phoneNum, location)
+
+        assert(mIndividualLocation.name == "Location 1")
+        assert(mIndividualLocation.address == "")
+        assert(mIndividualLocation.hours == "9am - 4pm")
+        assert(mIndividualLocation.phoneNum == "(555) 555-9430")
+        assert(mIndividualLocation.location.latitude == -73.970048)
+        assert(mIndividualLocation.location.longitude == 40.789752)
     }
 
 }
